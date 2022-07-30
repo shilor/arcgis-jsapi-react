55a1c0395e84066580f9786fc5d56fbea886b078
Shilo Rohlman <shilor@gmail.com>
2022-07-30 16:33:23 -0600

    add Denver map service

--- src/denvercrime/DenverCrimeApp.js
+++ src/denvercrime/DenverCrimeApp.js
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from "react";
 import MapView from "@arcgis/core/views/MapView";
 import WebMap from "@arcgis/core/WebMap";
+import BasemapToggle from "@arcgis/core/widgets/BasemapToggle";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 const DenverCrimeApp = () => {
   const mapDiv = useRef(null);
@@ -8,16 +10,32 @@ const DenverCrimeApp = () => {
   useEffect(() => {
     if (mapDiv.current) {
       /**
-       * Initialize application
+       * Initialize application and zoom to Denver
        */
       const webmap = new WebMap({
-        basemap: "streets",
+        basemap: "streets-vector",
       });
 
-      new MapView({
+      const mapView = new MapView({
         container: mapDiv.current,
         map: webmap,
+        center: [-104.9, 39.75],
+        zoom: 10,
       });
+
+      const basemapToggle = new BasemapToggle({
+        view: mapView,
+        nextBasemap: "hybrid",
+      });
+
+      mapView.ui.add(basemapToggle, "bottom-right");
+
+      //Neighborhoods feature layer hosted by City & County of Denver
+      const neighborhoodsLayer = new FeatureLayer({
+        url: "https://services1.arcgis.com/zdB7qR0BtYrg0Xpl/ArcGIS/rest/services/ODC_ADMN_NEIGHBORHOOD_A/FeatureServer/13",
+      });
+
+      webmap.add(neighborhoodsLayer);
     }
   }, []);
 
