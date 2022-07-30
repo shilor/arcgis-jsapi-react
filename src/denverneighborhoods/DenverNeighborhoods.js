8174ec124a143cf212ffcda92be2416d9278be0b
Shilo Rohlman <shilor@gmail.com>
2022-07-30 17:50:08 -0600

    add popup and search to neighborhood map

--- src/denverneighborhoods/DenverNeighborhoods.js
+++ src/denverneighborhoods/DenverNeighborhoods.js
@@ -3,6 +3,7 @@ import MapView from "@arcgis/core/views/MapView";
 import WebMap from "@arcgis/core/WebMap";
 import BasemapToggle from "@arcgis/core/widgets/BasemapToggle";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import Search from "@arcgis/core/widgets/Search";
 
 import "./DenverNeighborhoods.css";
 
@@ -32,12 +33,30 @@ const DenverNeighborhoods = () => {
 
       mapView.ui.add(basemapToggle, "bottom-right");
 
+      // Define a pop-up for Neighborhoods
+      const popupNeighborhood = {
+        title: "Neighborhood",
+        content: "<b>Neighborhood:</b> {NBHD_NAME}<br>",
+      };
+
+      webmap.add(popupNeighborhood);
+
       //Neighborhoods feature layer hosted by City & County of Denver
       const neighborhoodsLayer = new FeatureLayer({
         url: "https://services1.arcgis.com/zdB7qR0BtYrg0Xpl/ArcGIS/rest/services/ODC_ADMN_NEIGHBORHOOD_A/FeatureServer/13",
+        opacity: 0.5,
+        outFields: ["NBHD_ID", "NBHD_NAME"],
+        popupTemplate: popupNeighborhood,
       });
 
       webmap.add(neighborhoodsLayer);
+
+      //Add Search widget
+      const search = new Search({
+        view: mapView,
+      });
+
+      mapView.ui.add(search, "top-right");
     }
   }, []);
 
