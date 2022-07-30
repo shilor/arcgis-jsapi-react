f7aa4582551aeacd671fcba0edfb74a4098eab50
Shilo Rohlman <shilor@gmail.com>
2022-07-29 22:39:46 -0600

    fix router for relative paths

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import DenverCrimeApp from "./denvercrime/DenverCrimeApp";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="neighborhoods" element={<DenverNeighborhoods />} />
